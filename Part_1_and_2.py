a8e328be743009c3823926443c92620a6208f003
Manish Kamble <45763055+zapod838@users.noreply.github.com>
2024-03-31 18:06:12 +0530

    Update Part_1_and_2.py

--- Part_1_and_2.py
+++ Part_1_and_2.py
@@ -1,2 +1,6 @@
 #Python code will load soon...
-theek h
+'Date': dates,
+'Nominal_Exchange_Rate': nominal_exchange_rate,
+'Real_Exchange_Rate': real_exchange_rate,
+'CPI_Home': cpi_home,
+'CPI_Foreign': cpi_foreign
